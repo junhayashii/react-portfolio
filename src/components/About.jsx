920a0709b9590be5c7055eb3cf8a1b2fbe9b8a14
Jun Hayashi <kevin.jun.hayashi@gmail.com>
2024-07-24 17:35:05 -0300

    About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { aboutData } from "../data/aboutData";
+
+const SectionItem = ({ date, title, description, details, skills }) => (
+  <div className="section-item">
+    <div className="section-content">
+      <span className="date">{date}</span>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      {details && (
+        <ul className="details">
+          {details.map((detail, index) => (
+            <li key={index}>{detail}</li>
+          ))}
+        </ul>
+      )}
+      {skills && (
+        <div className="skills">
+          <ul>
+            {skills.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  </div>
+);
 
 function About() {
-  return <div className="about"></div>;
+  useEffect(() => {
+    const items = document.querySelectorAll(".fade-in");
+    items.forEach((item, index) => {
+      item.style.animationDelay = `${index * 0.3}s`;
+    });
+  }, []);
+
+  const { education, experience } = aboutData;
+
+  return (
+    <section className="about">
+      <div className="about-container">
+        <div className="left-column">
+          <div className="introduction  fade-in">
+            <h2>About Me</h2>
+            <p>
+              Hello! I'm Jun, a passionate tech enthusiast with a love for
+              innovation. From a young age, I've been fascinated by how
+              technology can solve real-world problems, driving me to
+              continuously explore and learn.
+            </p>
+            <p>
+              Currently, I am focused on broadening my knowledge and skills in
+              various areas of technology, and I am eager to contribute to
+              impactful projects that address real-world needs.
+            </p>
+          </div>
+          <div className="education  fade-in">
+            <h2>Education</h2>
+            {education.map((item, index) => (
+              <SectionItem
+                key={index}
+                date={item.date}
+                title={item.title}
+                description={item.description}
+                details={item.details}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="right-column">
+          <div className="experience  fade-in">
+            <h2>Experience</h2>
+            {experience.map((item, index) => (
+              <SectionItem
+                key={index}
+                date={item.date}
+                title={item.title}
+                description={item.description}
+                details={item.details}
+                skills={item.skills}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
---
+++ src/data/aboutData.js
@@ -0,0 +1,38 @@
+export const aboutData = {
+  education: [
+    {
+      date: "2021 - 2024",
+      title: "University of São Paulo (USP)",
+      description: "Bachelor's degree in Information Systems.",
+      details: [
+        "Studied core technical subjects such as Algorithms, Data Structures, Operating Systems, Networks, Database Management and Artificial Intelligence.",
+        "Gained knowledge in other areas like Administration, Economy, and Statistics.",
+      ],
+    },
+  ],
+  experience: [
+    {
+      date: "2022 Jun - Dec",
+      title: "Media Accel",
+      description:
+        "Freelance role involving content creation for websites as a writer, reviewer, and project manager.",
+      details: [
+        "Created and formatted text focused on SEO analysis in WordPress.",
+        "Hired and trained freelance writers.",
+      ],
+      skills: ["SEO", "WordPress"],
+    },
+    {
+      date: "2024 Jan - Dec",
+      title: "AFETECH",
+      description:
+        "Participated in an internship focusing on advanced IT solutions, particularly in information security, data management, and cloud infrastructure.",
+      details: [
+        "Received training on BigID for data management and privacy.",
+        "Worked on creating and analyzing regular expressions for data classification.",
+        "Participated in meetings and developed project documentation.",
+      ],
+      skills: ["BigID", "Regex"],
+    },
+  ],
+};
