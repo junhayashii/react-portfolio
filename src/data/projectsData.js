949c32f5f0becf90ae9bc79ebee94eaf56f56c75
Jun Hayashi <kevin.jun.hayashi@gmail.com>
2024-07-31 21:36:12 -0300

    Deploy configuration fixed

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -14,7 +14,7 @@ export const projectsData = [
     ],
     skills: ["HTML", "CSS", "JavaScript", "React", "Git"],
     source: "https://github.com/junhayashii/react-portfolio",
-    link: "https://junhayashii.github.io/react-portfolio",
+    link: "https://junhayashii.github.io",
   },
   {
     title: "Project 2",
