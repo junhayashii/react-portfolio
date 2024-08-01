ccbe2034c15e9ec6b218acccefc2536cd565d0d0
Jun Hayashi <kevin.jun.hayashi@gmail.com>
2024-07-31 21:24:53 -0300

    Deploy configuration fixed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-portfolio",
+  base: "/react-portfolio/",
   css: {
     localsConvention: "camelCase",
   },
